1b56feb9cc47df97edcb8b28c214022e910de957
TaekyungHa <86tkstar@gmail.com>
2025-03-05 01:54:08 +0900

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/flower-card/",
+  base: "/flower-card-game/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
